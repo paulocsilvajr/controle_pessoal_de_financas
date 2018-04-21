29f9b1840be2c5f15d7b640c7379ded7c6c2b595
paulocsilvajr <pauluscave@gmail.com>
2018-04-21 09:27:13 -0300

    Melhorado script install_tools(envolvido em funções as funcionalidades)

--- install_tools.sh
+++ install_tools.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-sudo apt install mysql-workbench -y && echo "Instalado MySQL Workbench"
+is_root(){
+    if ! [ $UID -eq 0 ]; then
+        echo "Execute como root"
+        exit 1
+    fi
+}
+
+install_mysql-workbench(){
+    apt install mysql-workbench -y && echo -e "Instalado MySQL Workbench\n\n"
+}
+
+is_root
+
+install_mysql-workbench &&
+    echo "END OF LINE."

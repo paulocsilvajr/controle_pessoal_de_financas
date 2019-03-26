7c32469163d4fab24cc2601faf3e4e7c8e3c5860
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-03-26 10:52:22 -0300

    Adicionado test para models/erros/erros.go

---
+++ API/v1/models/erros/erros_test.go
@@ -0,0 +1,16 @@
+package erros
+
+import "testing"
+
+func TestErros(t *testing.T) {
+	erroTamanho := ErroTamanho("mensagem", 42)
+	if erroTamanho.Error() != "mensagem[42]" {
+		t.Error("Retorno incorreto, função erros.ErroTamanho()")
+	}
+
+	erroDetalhe := ErroDetalhe("mensagem", "Resposta para a vida, o universo e tudo mais")
+	if erroDetalhe.Error() != "mensagem[Resposta para a vida, o universo e tudo mais]" {
+		t.Error("Retorno incorreto, função erros.ErroDetalhe()")
+	}
+
+}

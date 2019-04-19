34d996142127650608333fd4ee3224ae737e8348
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-04-19 13:11:22 -0300

    Modificado script run para garantir que rodará como ROOT

--- API/v1/run.sh
+++ API/v1/run.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-clear ; go run main.go
+function gorun() {
+    # Usado função invocando o executável do Golang no diretório origem para garantir que rodará como root
+    /usr/local/go/bin/go run $1
+}
+
+clear ; gorun main.go

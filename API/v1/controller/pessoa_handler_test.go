b561aea81c3552297f067dca15bca600b8f4a13a
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-05-01 17:05:54 -0300

    Continuação de criação de testes em pessoa_handler_test

--- API/v1/controller/pessoa_handler_test.go
+++ API/v1/controller/pessoa_handler_test.go
@@ -1,6 +1,9 @@
 package controller
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPessoaIndex(t *testing.T) {
 	tokenPessoaAdmin, _ := getToken("teste01", "123456")
@@ -20,6 +23,82 @@ func TestPessoaIndex(t *testing.T) {
 	res, body, _ = get(rota, tokenPessoaComum)
 	status = res.StatusCode
 	if status != 200 {
+		t.Error(res, string(body))
+	}
+}
+
+func TestPessoaShow(t *testing.T) {
+	// retorno de dados de pessoa admin - 200
+	usuario := "teste01"
+	tokenPessoaAdmin, _ := getToken(usuario, "123456")
+	rota := fmt.Sprintf("/pessoas/%s", usuario)
+	res, body, err := get(rota, tokenPessoaAdmin)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	status := res.StatusCode
+	if status != 200 {
+		t.Error(res, string(body))
+	}
+
+	// retorno de dados de pessoa comum - 200
+	usuario = "paulo"
+	tokenPessoaComum, _ := getToken(usuario, "123456")
+	rota = fmt.Sprintf("/pessoas/%s", usuario)
+	res, body, _ = get(rota, tokenPessoaComum)
+	status = res.StatusCode
+	if status != 200 {
+		t.Error(res, string(body))
+	}
+
+	// usuário token diferente do usuário da rota - 500
+	usuario = "joao02"
+	tokenPessoaAdmin, _ = getToken("teste01", "123456")
+	rota = fmt.Sprintf("/pessoas/%s", usuario)
+	res, body, _ = get(rota, tokenPessoaAdmin)
+	status = res.StatusCode
+	if status != 500 {
+		t.Error(res, string(body))
+	}
+}
+
+func TestPessoaShowAdmin(t *testing.T) {
+	// retorno de dados de pessoa comum com token de admin - 200
+	admin := "teste01"
+	usuario := "joao02"
+	tokenPessoaAdmin, _ := getToken(admin, "123456")
+	rota := fmt.Sprintf("/pessoas/%s/%s", admin, usuario)
+	res, body, err := get(rota, tokenPessoaAdmin)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	status := res.StatusCode
+	if status != 200 {
+		t.Error(res, string(body))
+	}
+
+	// retorno de dados de pessoa comum com token comum - 500
+	usuario = "paulo"
+	tokenPessoaComun, _ := getToken(usuario, "123456")
+	rota = fmt.Sprintf("/pessoas/%s/%s", usuario, usuario)
+	res, body, _ = get(rota, tokenPessoaComun)
+	status = res.StatusCode
+	if status != 500 {
+		t.Error(res, string(body))
+	}
+
+	// rota somente para administradores - 500
+	usuario = "paulo"
+	usuario2 := "paulo2"
+	tokenPessoaComun, _ = getToken(usuario, "123456")
+	rota = fmt.Sprintf("/pessoas/%s/%s", usuario, usuario2)
+	res, body, _ = get(rota, tokenPessoaComun)
+	status = res.StatusCode
+	if status != 500 {
 		t.Error(res, string(body))
 	}
 }

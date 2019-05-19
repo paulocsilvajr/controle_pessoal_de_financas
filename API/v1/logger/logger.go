be74e867cf3b398cee24103486b9878c142ac8a8
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-05-19 13:34:07 -0300

    Adicionado função CriarDiretorioAbs em função GeraLogFS e retirado de ServerHTTPAndLog função CriarDiretorioSeNaoExistir

--- API/v1/logger/logger.go
+++ API/v1/logger/logger.go
@@ -10,7 +10,10 @@ import (
 	"time"
 )
 
-func ServeHttpAndLog(inner http.Handler, name string) http.Handler {
+const dirLOG = "logs"
+
+// ServeHTTPAndLog retorna o handler que serve/disponibiliza o HTTP para API
+func ServeHTTPAndLog(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			// w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -18,8 +21,6 @@ func ServeHttpAndLog(inner http.Handler, name string) http.Handler {
 			start := time.Now()
 
 			inner.ServeHTTP(w, r)
-
-			helper.CriarDiretorioSeNaoExistir("logs")
 
 			msg := fmt.Sprintf(
 				"%s\t%s\t%s\t%s",
@@ -33,9 +34,12 @@ func ServeHttpAndLog(inner http.Handler, name string) http.Handler {
 		})
 }
 
+// GeraLogFS gera um arquivo de log baseado na mensagem(msg) e data/hora informada. Logs são gerados no formato yyyyMMdd.log
 func GeraLogFS(msg string, startTime time.Time) {
+	dirAbsLOG := helper.CriarDiretorioAbs(dirLOG)
+
 	// log em arquivo
-	nomeArquivo := fmt.Sprintf("logs/%04d%02d%02d.log", startTime.Year(), startTime.Month(), startTime.Day())
+	nomeArquivo := fmt.Sprintf("%s/%04d%02d%02d.log", dirAbsLOG, startTime.Year(), startTime.Month(), startTime.Day())
 	arquivo, err := os.OpenFile(nomeArquivo, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Printf("Erro ao abrir arquivo de log[%s]", err)

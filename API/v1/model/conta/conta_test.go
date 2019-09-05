4bef3d918d518902e936926fee5349a9972dbc7a
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-09-04 21:40:25 -0300

    Adicionado teste funções New e NewConta de modelo Conta

--- API/v1/model/conta/conta_test.go
+++ API/v1/model/conta/conta_test.go
@@ -1,7 +1,9 @@
 package conta
 
 import (
+	"controle_pessoal_de_financas/API/v1/model/tipo_conta"
 	"testing"
+	"time"
 )
 
 func TestIConta(t *testing.T) {
@@ -19,6 +21,28 @@ func TestIConta(t *testing.T) {
 	if !ok {
 		t.Error(c4)
 	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Error(r)
+		}
+	}()
+
+	var ic3 IConta
+	ic3 = GetContaTest()
+	_ = ic3.String()
+	ic3.Repr()
+	ic3.VerificaAtributos()
+	ic3.Altera("Novo nome de conta", "Outro tipo de conta", "003", c4.Nome, "Alterando conta")
+	campos := map[string]string{
+		"nome":          "Novo nome de conta 02",
+		"nomeTipoConta": "Outro tipo de conta 02",
+		"codigo":        "004",
+		"contaPai":      "",
+		"comentario":    "Alterando conta 02"}
+	ic3.AlteraCampos(campos)
+	ic3.Ativa()
+	ic3.Inativa()
 }
 
 func TestConverteParaConta(t *testing.T) {
@@ -33,5 +57,151 @@ func TestConverteParaConta(t *testing.T) {
 	c4, err := converteParaConta(c2)
 	if err != nil {
 		t.Error(c4)
+	}
+}
+
+func TestNew(t *testing.T) {
+	tipoConta := tipo_conta.GetTipoContaTest()
+	nome := "Banco 02"
+	codigo := "002"
+	contaPai := ""
+	comentario := "teste de conta 02"
+	data := time.Now().Local()
+	estadoPadrao := true
+
+	c1 := New(nome, tipoConta.Nome, codigo, contaPai, comentario)
+
+	if c1.Nome != nome {
+		t.Error("Erro em função conta.New, atributo Nome", c1)
+	}
+
+	if c1.NomeTipoConta != tipoConta.Nome {
+		t.Error("Erro em função conta.New, atributo NomeTipoConta", c1)
+	}
+
+	if c1.Codigo != codigo {
+		t.Error("Erro em função conta.New, atributo Codigo", c1)
+	}
+
+	if c1.ContaPai != contaPai {
+		t.Error("Erro em função conta.New, atributo ContaPai", c1)
+	}
+
+	if c1.Comentario != comentario {
+		t.Error("Erro em função conta.New, atributo Comentario", c1)
+	}
+
+	if c1.DataCriacao.Unix() != data.Unix() {
+		t.Error("Erro em função conta.New, atributo DataCriacao", c1)
+	}
+
+	if c1.DataModificacao.Unix() != data.Unix() {
+		t.Error("Erro em função conta.New, atributo DataModificacao", c1)
+	}
+
+	if c1.Estado != estadoPadrao {
+		t.Error("Erro em função conta.New, atributo Estado", c1)
+	}
+}
+
+func TestNewConta(t *testing.T) {
+	tipoConta := tipo_conta.GetTipoContaTest()
+	nome := "Banco 02"
+	codigo := "002"
+	contaPai := ""
+	comentario := "teste de conta 02"
+	data := time.Now().Local()
+	estadoPadrao := true
+
+	c1, err := NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+
+	if c1.Nome != nome {
+		t.Error("Erro em função conta.New, atributo Nome", c1)
+	}
+
+	if c1.NomeTipoConta != tipoConta.Nome {
+		t.Error("Erro em função conta.New, atributo NomeTipoConta", c1)
+	}
+
+	if c1.Codigo != codigo {
+		t.Error("Erro em função conta.New, atributo Codigo", c1)
+	}
+
+	if c1.ContaPai != contaPai {
+		t.Error("Erro em função conta.New, atributo ContaPai", c1)
+	}
+
+	if c1.Comentario != comentario {
+		t.Error("Erro em função conta.New, atributo Comentario", c1)
+	}
+
+	if c1.DataCriacao.Unix() != data.Unix() {
+		t.Error("Erro em função conta.New, atributo DataCriacao", c1)
+	}
+
+	if c1.DataModificacao.Unix() != data.Unix() {
+		t.Error("Erro em função conta.New, atributo DataModificacao", c1)
+	}
+
+	if c1.Estado != estadoPadrao {
+		t.Error("Erro em função conta.New, atributo Estado", c1)
+	}
+
+	if err != nil {
+		t.Error(err, c1)
+	}
+
+	comentario = ""
+	c2, err := NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Comentário inválido[0 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	comentario = "Descrição de conta muito longa para dar erro em teste unitário de modelo Conta ......... ........... ............. ............. ..................."
+	c2, err = NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Comentário inválido[151 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	contaPai = "Nome de Conta Pai muito longa para dar erro em teste unitário de modelo Conta"
+	c2, err = NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Nome da Conta pai inválido[78 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	codigo = "12345678901234567890"
+	c2, err = NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Código inválido[20 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	codigo = "ABCD123456789-20000101"
+	c2, err = NewConta(nome, tipoConta.Nome, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Código inválido[22 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	nomeTipoConta := ""
+	c2, err = NewConta(nome, nomeTipoConta, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Nome do Tipo da Conta inválido[0 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	nomeTipoConta = "Nome de Tipo de Conta muito longa para dar erro em teste unitário de modelo Conta"
+	c2, err = NewConta(nome, nomeTipoConta, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Nome do Tipo da Conta inválido[82 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	nome = ""
+	c2, err = NewConta(nome, nomeTipoConta, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Nome inválido[0 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
+	}
+
+	nome = "Nome muito longo para dar erro em teste unitário de modelo Conta"
+	c2, err = NewConta(nome, nomeTipoConta, codigo, contaPai, comentario)
+	if err.Error() != "Tamanho de campo Nome inválido[65 caracter(es)]" {
+		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
 	}
 }

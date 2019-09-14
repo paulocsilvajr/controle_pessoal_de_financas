90364c4b597bc7159d5d73b60bf9e6c9bf323df4
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-09-14 09:23:21 -0300

    Adicionado testes para funções GetContaTest, String, Repr, Altera de modelo Conta

--- API/v1/model/conta/conta_test.go
+++ API/v1/model/conta/conta_test.go
@@ -205,3 +205,98 @@ func TestNewConta(t *testing.T) {
 		t.Error("Erro em função conta.NewConta, nao retornou o erro esperado", c2, err)
 	}
 }
+
+func TestGetContaTeste(t *testing.T) {
+	nome := "Ativos Teste 01"
+	nomeTipoConta := "banco teste 01"
+	codigo := "001"
+	contaPai := ""
+	comentario := "teste de Conta"
+	criacao := time.Date(2000, 2, 1, 12, 30, 0, 0, new(time.Location))
+	modificacao := criacao
+	estado := true
+
+	c1 := GetContaTest()
+
+	if c1.Nome != nome {
+		t.Error("Erro em função conta.GetContaTest, atributo Nome", c1)
+	}
+
+	if c1.NomeTipoConta != nomeTipoConta {
+		t.Error("Erro em função conta.GetContaTest, atributo NomeTipoConta", c1)
+	}
+
+	if c1.Codigo != codigo {
+		t.Error("Erro em função conta.GetContaTest, atributo Codigo", c1)
+	}
+
+	if c1.ContaPai != contaPai {
+		t.Error("Erro em função conta.GetContaTest, atributo ContaPai", c1)
+	}
+
+	if c1.Comentario != comentario {
+		t.Error("Erro em função conta.GetContaTest, atributo Comentario", c1)
+	}
+
+	if c1.DataCriacao.Unix() != criacao.Unix() {
+		t.Error("Erro em função conta.GetContaTest, atributo DataCriacao", c1)
+	}
+
+	if c1.DataModificacao.Unix() != modificacao.Unix() {
+		t.Error("Erro em função conta.GetContaTest, atributo DataModificacao", c1)
+	}
+
+	if c1.Estado != estado {
+		t.Error("Erro em função conta.GetContaTest, atributo Estado", c1)
+	}
+}
+
+func TestString(t *testing.T) {
+	c1 := GetContaTest()
+
+	if c1.String() != "Ativos Teste 01	banco teste 01	001		teste de Conta	01/02/2000 12:30:00	01/02/2000 12:30:00	ativo" {
+		t.Error("Erro em função conta.String", c1.String())
+	}
+}
+
+func TestRepr(t *testing.T) {
+	c1 := GetContaTest()
+
+	if c1.Repr() != "Ativos Teste 01	banco teste 01	001		teste de Conta	2000-02-01 12:30:00 +0000 UTC	2000-02-01 12:30:00 +0000 UTC	true" {
+		t.Error("Erro em função conta.Repr", c1.Repr())
+	}
+}
+
+func TestAltera(t *testing.T) {
+	c1 := GetContaTest()
+
+	err := c1.Altera("Ativos Teste 02", "banco teste 02", "002", "Ativos", "teste de Conta 02")
+	if err != nil {
+		t.Error(err, c1)
+	}
+
+	err = c1.Altera("Nome muito longo para dar erro em teste unitário de modelo Conta", "banco teste 02", "002", "Ativos", "teste de Conta 02")
+	if err.Error() != "Tamanho de campo Nome inválido[65 caracter(es)]" {
+		t.Error(err, c1)
+	}
+
+	err = c1.Altera("Ativos Teste 02", "Nome de Tipo de Conta muito longa para dar erro em teste unitário de modelo Conta", "002", "Ativos", "teste de Conta 02")
+	if err.Error() != "Tamanho de campo Nome do Tipo da Conta inválido[82 caracter(es)]" {
+		t.Error(err, c1)
+	}
+
+	err = c1.Altera("Ativos Teste 02", "banco teste 02", "ABCD123456789-20000101", "Ativos", "teste de Conta 02")
+	if err.Error() != "Tamanho de campo Código inválido[22 caracter(es)]" {
+		t.Error(err, c1)
+	}
+
+	err = c1.Altera("Ativos Teste 02", "banco teste 02", "002", "Nome de Conta Pai muito longa para dar erro em teste unitário de modelo Conta", "teste de Conta 02")
+	if err.Error() != "Tamanho de campo Nome da Conta pai inválido[78 caracter(es)]" {
+		t.Error(err, c1)
+	}
+
+	err = c1.Altera("Ativos Teste 02", "banco teste 02", "002", "Ativos", "Descrição de conta muito longa para dar erro em teste unitário de modelo Conta ......... ........... ............. ............. ...................")
+	if err.Error() != "Tamanho de campo Comentário inválido[151 caracter(es)]" {
+		t.Error(err, c1)
+	}
+}

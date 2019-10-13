b4dc3a8fd115ff58c714c2531e45f55242944706
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2019-10-13 10:47:31 -0300

    Adicionado em numero_helper uma função para verificar valores e corrigido docstring de MonetarioFormatado

--- API/v1/helper/numero_helper.go
+++ API/v1/helper/numero_helper.go
@@ -2,7 +2,16 @@ package helper
 
 import "fmt"
 
-// MonetarioFormatado retorna uma string baseado em um número decimal(float32) no formato de 3 casas decimais
+// MonetarioFormatado retorna uma string baseado em um número decimal(float64) no formato de 3 casas decimais
 func MonetarioFormatado(valor float64) string {
 	return fmt.Sprintf("%.3f", valor)
+}
+
+// VerificaValor retorna um erro caso o parâmetro valor informado seja < 0. O primeiro parâmetro é o nome do campo testado, para exibir este nome na mensagem de erro, caso ocorra
+func VerificaValor(campo string, valor float64) (err error) {
+	if valor < 0 {
+		err = fmt.Errorf("O campo %s deve ser >= 0", campo)
+	}
+
+	return
 }

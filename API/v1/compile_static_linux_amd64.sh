6a130615fad558d454eb3c2cf90fbc96ab3ba5f0
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-03-15 21:01:42 -0300

    Alterado script compilador para linux AMD64 adicionado parâmetro para compilação estática

--- API/v1/compile_static_linux_amd64.sh
+++ API/v1/compile_static_linux_amd64.sh
@@ -20,9 +20,6 @@ fi
 mkdir -p $PASTA 2> /dev/null
 
 echo -e "Compilando para $GOOS:$GOARCH\n"
-go build -v -o $ARQUIVO -a -ldflags '-extldflags "-static" -s'
+go build -v -o $ARQUIVO -a -installsuffix cgo -ldflags '-extldflags "-static" -s'
 
 cp -vr keys $PASTA
-
-# # Caso não compile estáticamente, rode o comando abaixo no terminal e depois reexecute este script
-# go build -v -a -ldflags '-extldflags "-static" -s'

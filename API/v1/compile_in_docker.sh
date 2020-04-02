792479aebd84cf37db13428794b59307ee85bab9
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-04-02 08:33:26 -0300

    Alterado variável DIR_ABS
    Alterado o diretório home absoluto informado por variável $HOME

--- API/v1/compile_in_docker.sh
+++ API/v1/compile_in_docker.sh
@@ -4,6 +4,6 @@
 # controle_pessoal_de_financas de acordo com a localização no seu computador.
 # Geralmente localizado na pasta 'go' na home se seu usuário. Exemplo:
 # /home/seu-usuario/go/src/controle_pessoal_de_financas
-DIR_ABS=/home/kdepaulo/go/src/controle_pessoal_de_financas
+DIR_ABS=$HOME/go/src/controle_pessoal_de_financas
 
 docker run -it --rm -v $DIR_ABS:/go/src/controle_pessoal_de_financas golang_custom:controle_pessoal_de_financas ./controle_pessoal_de_financas/API/v1/compile_static_linux_amd64.sh

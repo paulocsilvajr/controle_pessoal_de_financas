f3925b88186019fb6860caafd85e74a28891cefc
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-06-17 12:01:07 -0300

    Adicionado em css de home formatação de card sobre informacões da sessão. Alterado view home para usar a classe css criada .informacoes-sessao

--- WEB/app/controle-pessoal-financas-laravel/resources/views/home.blade.php
+++ WEB/app/controle-pessoal-financas-laravel/resources/views/home.blade.php
@@ -6,9 +6,9 @@
 
 @section('conteudo')
     <h1 class="mensagem-inicial text-center">Bem vindo {{ $usuario ?? '' }}</h1>
-    <p class="text-center">Use a barra de navegação para acessar as funcionalidades do sistema</p>
+    <p class="text-center mt-3">Use a barra de navegação para acessar as funcionalidades do sistema</p>
 
-    <div class="card" style="width: 18rem;">
+    <div class="card informacoes-sessao" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">Dados em sessão</h5>
             <p class="card-text">senha: {{ $usuario }}</p>

d7623c6960b3e55b36aad54ebda43ef5ac934466
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-08-14 14:54:39 -0300

    Adicionado script para instalar vue integrado com laravel e o nodejs/npm

---
+++ WEB/app/controle-pessoal-financas-laravel/install_vuejs_in_laravel.sh
@@ -0,0 +1,10 @@
+# nodejs/npm atualizado via snap
+sudo snap install node --classic --channel=8
+
+composer require laravel/ui
+php artisan ui vue
+npm install
+
+# Para compilar, use o comando abaixo
+# npm run dev
+

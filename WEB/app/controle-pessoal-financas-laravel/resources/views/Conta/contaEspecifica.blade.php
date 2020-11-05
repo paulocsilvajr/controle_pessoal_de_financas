22adb2601de39ac12b005150c3306bbab292dcf3
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-11-05 15:12:50 -0300

    Iniciado criação de interface de contaEspecifica.blade usando icone de Font Awesome

--- WEB/app/controle-pessoal-financas-laravel/resources/views/Conta/contaEspecifica.blade.php
+++ WEB/app/controle-pessoal-financas-laravel/resources/views/Conta/contaEspecifica.blade.php
@@ -6,7 +6,18 @@
 
 @section('conteudo')
     <div class="container">
-        <p class="titulo">{{ $nomeConta }}</p>
+        <div class="row margem-navbar-conteudo">
+            <div class="col-sm-6 col-md-10 titulo-conta-sm">
+                <h1 class="text-center" style="margin: auto">{{ ucfirst($nomeConta) }}</h1>
+            </div>
+            <div class="col-sm-6 col-md-2 d-flex align-items-center">
+                <button class="btn btn-primary btn-block">
+                    Novo
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
+        </div>
+        <p>Continua...</p>
     </div>
 @endsection
 

72220701efcb0e88c6869f13b8d918c514dd05d5
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-11-24 16:36:13 -0300

    Removido código comentado de home.blade que já foi substituído

--- WEB/app/controle-pessoal-financas-laravel/resources/views/home.blade.php
+++ WEB/app/controle-pessoal-financas-laravel/resources/views/home.blade.php
@@ -42,25 +42,6 @@
         </div>
     </div>
 
-    {{-- <h1 class="mensagem-inicial text-center">Bem vindo {{ $usuario ?? '' }}</h1> --}}
-    {{-- <p class="text-center mt-3">Use a barra de navegação para acessar as funcionalidades do sistema</p> --}}
-
-    {{-- @if (env('APP_DEBUG', false))
-        <div class="card informacoes-sessao" style="width: 20rem;">
-            <div class="card-body">
-                <h5 class="card-title">Dados em sessão</h5>
-                <p class="card-text">usuário: {{ $usuario }}</p>
-                <p class="card-text">senha: {{ $senha }}</p>
-                <p class="card-text">token: {{ $tokenParcial }}</p>
-                <p class="card-text">logado: {{ $logado }}</p>
-            </div>
-        </div>
-    @endif --}}
-
-    {{-- <div id="app">
-        <exemplo></exemplo>
-    </div> --}}
-
 @endsection
 
 @section('script')

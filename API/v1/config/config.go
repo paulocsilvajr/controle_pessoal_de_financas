d8c55f8a0d899731418944fa09465d09906ed821
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-12-21 12:01:58 -0300

    Adicionado novo campo em struct rota de config.go e atribuido string vazia em cada entrada de variável Rotas

--- API/v1/config/config.go
+++ API/v1/config/config.go
@@ -1,13 +1,14 @@
 package config
 
 import (
-	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/helper"
-	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/logger"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path"
 	"time"
+
+	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/helper"
+	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/logger"
 )
 
 // https://play.golang.org/p/6dX5SMdVtr
@@ -16,7 +17,7 @@ const diretorioLog = "config"
 const arquivoLog = "config.json"
 
 type rota struct {
-	Tipo, Rota, Descricao string
+	Tipo, Rota, Descricao, Documentacao string
 }
 
 type rotas map[string]rota
@@ -25,161 +26,198 @@ func (r rotas) Len() int {
 	return len(r)
 }
 
+func (r rotas) DefineDocumentacao(rota, doc string) {
+	var temp = r[rota]
+	temp.Documentacao = doc
+	r[rota] = temp
+}
+
 // Rotas é uma variável que guarda todas as rotas definidas da API. Usa o tipo interno rota{Tipo, Rota, Descricao} em cada elemento desse hashMap rotas map[string]rota
 var Rotas = rotas{
 	"API": rota{
 		"GET",
 		"/API",
 		"",
+		"",
 	},
 	"Index": rota{
 		"GET",
 		"/",
+		"",
 		"",
 	},
 	"Login": rota{
 		"POST",
 		"/login/{usuario}",
 		`Body: {"usuario":"?",  "senha":"?"}`,
+		"",
 	},
 	"TokenValido": rota{
 		"GET",
 		"/token",
+		"",
 		"",
 	},
 	"PessoaIndex": rota{
 		"GET",
 		"/pessoas",
 		"",
+		"",
 	},
 	"PessoaShow": rota{
 		"GET",
 		"/pessoas/{usuario}",
+		"",
 		"",
 	},
 	"PessoaShowAdmin": rota{
 		"GET",
 		"/pessoas/{usuarioAdmin}/{usuario}",
 		"",
+		"",
 	},
 	"PessoaCreate": rota{
 		"POST",
 		"/pessoas",
 		`Body: {"cpf":"?",  "nome_completo":"?", "usuario":"?", "senha":"?", "email":"?"[, "administrador": ?]}`,
+		"",
 	},
 	"PessoaRemove": rota{
 		"DELETE",
 		"/pessoas/{usuario}",
+		"",
 		"",
 	},
 	"PessoaAlter": rota{
 		"PUT",
 		"/pessoas/{usuario}",
 		`Body: {"cpf":"?",  "nome_completo":"?", "usuario":"?", "senha":"?", "email":"?"}`,
+		"",
 	},
 	"PessoaEstado": rota{
 		"PUT",
 		"/pessoas/{usuario}/estado",
 		`Body: {"estado": ?}`,
+		"",
 	},
 	"PessoaAdmin": rota{
 		"PUT",
 		"/pessoas/{usuario}/admin",
 		`Body: {"administrador": ?}`,
+		"",
 	},
 	"TipoContaIndex": rota{
 		"GET",
 		"/tipos_conta",
+		"",
 		"",
 	},
 	"TipoContaShow": rota{
 		"GET",
 		"/tipos_conta/{tipoConta}",
 		"",
+		"",
 	},
 	"TipoContaCreate": rota{
 		"POST",
 		"/tipos_conta",
 		`Body: {"nome":"?",  "descricao_debito":"?", "descricao_credito":"?"}`,
+		"",
 	},
 	"TipoContaRemove": rota{
 		"DELETE",
 		"/tipos_conta/{tipoConta}",
+		"",
 		"",
 	},
 	"TipoContaAlter": rota{
 		"PUT",
 		"/tipos_conta/{tipoConta}",
 		`Body: {["nome":"?",]  "descricao_debito":"?", "descricao_credito":"?"}`,
+		"",
 	},
 	"TipoContaEstado": rota{
 		"PUT",
 		"/tipos_conta/{tipoConta}/estado",
 		`Body: {"estado": ?}`,
+		"",
 	},
 	"ContaIndex": rota{
 		"GET",
 		"/contas",
+		"",
 		"",
 	},
 	"ContaShow": rota{
 		"GET",
 		"/contas/{conta}",
 		"",
+		"",
 	},
 	"ContaCreate": rota{
 		"POST",
 		"/contas",
 		`Body: {"nome":"?",  "nome_tipo_conta":"?", "codigo":"?", "conta_pai":"?", "comentario":"?"}`,
+		"",
 	},
 	"ContaRemove": rota{
 		"DELETE",
 		"/contas/{conta}",
+		"",
 		"",
 	},
 	"ContaAlter": rota{
 		"PUT",
 		"/contas/{conta}",
 		`Body: {["nome":"?",]  "nome_tipo_conta":"?", "codigo":"?", "conta_pai":"?", "comentario":"?"}`,
+		"",
 	},
 	"ContaEstado": rota{
 		"PUT",
 		"/contas/{conta}/estado",
 		`Body: {"estado": ?}`,
+		"",
 	},
 	"LancamentoIndex": rota{
 		"GET",
 		"/lancamentos",
+		"",
 		"",
 	},
 	"LancamentoShow": rota{
 		"GET",
 		"/lancamentos/{lancamento}",
 		"",
+		"",
 	},
 	"LancamentoCreate": rota{
 		"POST",
 		"/lancamentos",
 		`Body: {"cpf_pessoa":"?", "nome_conta_origem":"?", "data":"?", "numero":"?", "descricao":"?", "nome_conta_destino":"?", "debito":?, "credito":?}`,
+		"",
 	},
 	"LancamentoRemove": rota{
 		"DELETE",
 		"/lancamentos/{lancamento}",
+		"",
 		"",
 	},
 	"LancamentoAlter": rota{
 		"PUT",
 		"/lancamentos/{lancamento}/{origen}/{destino}",
 		`Body: {["cpf_pessoa":"?",] "nome_conta_origem":"?", "data":"?", "numero":"?", "descricao":"?", "nome_conta_destino":"?", "debito":?, "credito":?}`,
+		"",
 	},
 	"LancamentoEstado": rota{
 		"PUT",
 		"/lancamentos/{lancamento}/estado",
 		`Body: {"estado": ?}`,
+		"",
 	},
 	"LancamentoPorConta": rota{
 		"GET",
 		"/lancamentos_conta/{conta}",
+		"",
 		"",
 	},
 }

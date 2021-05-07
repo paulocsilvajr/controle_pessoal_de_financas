6bd7f5cc80849c7070c85e8251d152010a42e3ef
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-05-07 16:03:46 -0300

    Adicionado GORM em script de instalação de dependências

--- API/v1/install_dependencies.sh
+++ API/v1/install_dependencies.sh
@@ -11,3 +11,7 @@ install github.com/gorilla/mux
 install github.com/lib/pq
 install github.com/auth0/go-jwt-middleware
 install github.com/jedib0t/go-pretty/table
+
+# GORM(https://gorm.io/docs/index.html)
+install gorm.io/gorm
+install gorm.io/driver/postgres

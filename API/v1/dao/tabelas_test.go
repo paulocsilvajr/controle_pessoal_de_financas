a7f25ec71dfbd268e6c00d926c92b665aa377588
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-05-24 22:17:46 -0300

    Criado testes de CRUD para TLancamento e TDetalheLancamento. Extraido criação de variáveis locais ref a structs em funções. Criado função init para criar bando de dados de teste e retornar a coneção db2 ref a este banco criado

--- API/v1/dao/tabelas_test.go
+++ API/v1/dao/tabelas_test.go
@@ -2,14 +2,23 @@ package dao
 
 import (
 	"testing"
+	"time"
 
 	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/model/conta"
+	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/model/detalhe_lancamento"
+	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/model/lancamento"
 	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/model/pessoa"
 	"github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/model/tipo_conta"
 	"gorm.io/gorm"
 )
 
-var db2 *gorm.DB = GetDB02()
+var db2 *gorm.DB
+
+func init() {
+	CreateDBParaTestes()
+
+	db2 = GetDB02ParaTestes()
+}
 
 func TestCriarTabelaPessoa(t *testing.T) {
 	err := CriarTabelaPessoa(db2)
@@ -20,13 +29,7 @@ func TestCriarTabelaPessoa(t *testing.T) {
 
 func TestCRUDPessoa(t *testing.T) {
 	// Criar - INSERT
-	p1 := pessoa.TPessoa{
-		Cpf:          "12345678910",
-		NomeCompleto: "Teste 01",
-		Usuario:      "teste01",
-		Senha:        "123456",
-		Email:        "teste01@email.com",
-	}
+	p1 := getTPessoa1()
 
 	p2 := pessoa.TPessoa{
 		Cpf:          "10987654321",
@@ -94,11 +97,7 @@ func TestCriarTabelaTipoConta(t *testing.T) {
 
 func TestCRUDTipoConta(t *testing.T) {
 	// Criar - INSERT
-	tc1 := tipo_conta.TTipoConta{
-		Nome:             "banco",
-		DescricaoDebito:  "saque",
-		DescricaoCredito: "depósito",
-	}
+	tc1 := getTTipoConta1()
 
 	tc2 := tipo_conta.TTipoConta{
 		Nome:             "carteira",
@@ -170,26 +169,14 @@ func TestCriarTabelaConta(t *testing.T) {
 
 func TestCRUDTabelaConta(t *testing.T) {
 	// Criar - INSERT
-	tc1 := &tipo_conta.TTipoConta{
-		Nome:             "banco",
-		DescricaoDebito:  "saque",
-		DescricaoCredito: "depósito",
-	}
+	tc := getTTipoConta1()
+	tc1 := &tc
 
-	c1 := &conta.TConta{
-		Nome:          "Juros",
-		NomeTipoConta: tc1.Nome,
-		Codigo:        setNullString("001"),
-		Comentario:    setNullString("teste de conta 001 em banco"),
-	}
+	c := getTConta1(*tc1)
+	c1 := &c
 
-	c2 := &conta.TConta{
-		Nome:          "Bradesco - juros",
-		NomeTipoConta: tc1.Nome,
-		Codigo:        setNullString("002"),
-		ContaPai:      setNullString(c1.Nome),
-		Comentario:    setNullString("teste de conta 002 em banco"),
-	}
+	c = getTConta2(tc, c)
+	c2 := &c
 
 	err := db2.Create(tc1).Error
 	if err != nil {
@@ -267,9 +254,224 @@ func TestCriarTabelaLancamento(t *testing.T) {
 	}
 }
 
+func TestCRUDTabelaLancamento(t *testing.T) {
+	// Criar - INSERT
+	p1 := getTPessoa1()
+
+	err := db2.Create(&p1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	l := getTLancamento1(p1)
+	l1 := &l
+
+	err = db2.Create(l1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Alterar - UPDATE
+	l1.Data = time.Date(2022, time.Month(12), 24, 0, 0, 0, 0, time.UTC)
+	l1.Numero = setNullString("010101")
+	l1.Descricao = "Lançamento 010101"
+
+	err = db2.Save(l1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Consultar - SELECT
+	id := l1.ID
+	l1 = nil
+
+	err = db2.Where("id = ?", id).First(&l1).Error
+	// t.Error(l1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Remover - DELETE
+	err = db2.Delete(l1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(p1).Error
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestCriarTabelaDetalheLancamento(t *testing.T) {
 	err := CriarTabelaDetalheLancamento(db2)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestCRUDTabelaDetalheLancamento(t *testing.T) {
+	// Criar - INSERT
+	p1 := getTPessoa1()
+	err := db2.Create(&p1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	l1 := getTLancamento1(p1)
+	err = db2.Create(&l1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	tc1 := getTTipoConta1()
+	err = db2.Create(&tc1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	c1 := getTConta1(tc1)
+	err = db2.Create(&c1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	c2 := getTConta2(tc1, c1)
+	err = db2.Create(&c2).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	dl1 := detalhe_lancamento.TDetalheLancamento{
+		IDLancamento: l1.ID,
+		NomeConta:    c1.Nome,
+		Debito:       setNullFloat64(0),
+		Credito:      setNullFloat64(101),
+	}
+
+	dl2 := detalhe_lancamento.TDetalheLancamento{
+		IDLancamento: l1.ID,
+		NomeConta:    c2.Nome,
+		Debito:       setNullFloat64(101),
+		Credito:      setNullFloat64(0),
+	}
+
+	err = db2.Create(&dl1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Create(&dl2).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Alterar - UPDATE
+	dl1.Debito = setNullFloat64(200)
+	dl1.Credito = setNullFloat64(0)
+
+	dl2.Debito = dl1.Credito
+	dl2.Credito = dl1.Debito
+
+	err = db2.Save(&dl1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Save(&dl2).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Consultar - SELECT
+	id := dl1.IDLancamento
+	dls := []detalhe_lancamento.TDetalheLancamento{}
+
+	err = db2.Where("id_lancamento = ?", id).Find(&dls).Error
+	// t.Error(dls, dl1, dl2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Remover - DELETE
+	err = db2.Delete(dl1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(dl2).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(c1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(c2).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(tc1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(l1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = db2.Delete(p1).Error
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func getTPessoa1() pessoa.TPessoa {
+	return pessoa.TPessoa{
+		Cpf:          "12345678910",
+		NomeCompleto: "Teste 01",
+		Usuario:      "teste01",
+		Senha:        "123456",
+		Email:        "teste01@email.com",
+	}
+}
+
+func getTTipoConta1() tipo_conta.TTipoConta {
+	return tipo_conta.TTipoConta{
+		Nome:             "banco",
+		DescricaoDebito:  "saque",
+		DescricaoCredito: "depósito",
+	}
+}
+
+func getTConta1(tc tipo_conta.TTipoConta) conta.TConta {
+	return conta.TConta{
+		Nome:          "Juros",
+		NomeTipoConta: tc.Nome,
+		Codigo:        setNullString("001"),
+		Comentario:    setNullString("teste de conta 001 em banco"),
+	}
+}
+
+func getTConta2(tc tipo_conta.TTipoConta, c conta.TConta) conta.TConta {
+	return conta.TConta{
+		Nome:          "Bradesco - juros",
+		NomeTipoConta: tc.Nome,
+		Codigo:        setNullString("002"),
+		ContaPai:      setNullString(c.Nome),
+		Comentario:    setNullString("teste de conta 002 em banco"),
+	}
+}
+
+func getTLancamento1(p pessoa.TPessoa) lancamento.TLancamento {
+	return lancamento.TLancamento{
+		CpfPessoa: p.Cpf,
+		Numero:    setNullString("101010"),
+		Descricao: "Lançamento teste 101010",
 	}
 }

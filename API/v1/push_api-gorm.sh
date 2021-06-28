9921fd2124531923a4701e8496cd2bafaebb5405
Paulo C <pauluscave@gmail.com>
2021-06-28 16:11:31 -0300

    Melhorado scripts de push para repositórios remotos para verificar se estão na branch correta

--- API/v1/push_api-gorm.sh
+++ API/v1/push_api-gorm.sh
@@ -1,1 +1,11 @@
-git push origin api-gorm && git push bitbucket api-gorm && git push github api-gorm
+#!/usr/bin/env bash
+
+CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH="api-gorm"
+
+if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
+    git push origin api-gorm && git push bitbucket api-gorm && git push github api-gorm
+else
+    echo "Altere para a branch '$BRANCH'(git checkout api-gorm) para executar esse script"
+    exit 1
+fi
--- API/v1/push_api.sh
+++ API/v1/push_api.sh
@@ -1,1 +1,12 @@
-git push origin api && git push bitbucket api && git push github api
+#!/usr/bin/env bash
+
+CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH="api"
+
+if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
+    git push origin api && git push bitbucket api && git push github api
+else
+    echo "Altere para a branch '$BRANCH'(git checkout api) para executar esse script"
+    exit 1
+fi
+

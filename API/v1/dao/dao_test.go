510a930f1eac8a29b9a66437e5e45e2001213d74
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-07-05 14:14:15 -0300

    Removido comentário para teste de função GetTemplateQuery - testedo - OK

--- API/v1/dao/dao_test.go
+++ API/v1/dao/dao_test.go
@@ -18,6 +18,26 @@ func TestGetDB02(t *testing.T) {
 	}
 }
 
+func TestGetTemplateQuery(t *testing.T) {
+	sql := `
+SELECT
+	{{.cpf}}, {{.nomeCompleto}}, {{.usuario}}, {{.senha}}, {{.email}}, {{.dataCriacao}}, {{.dataModificacao}}, {{.estado}}
+FROM
+	{{.tabela}}
+`
+
+	query := getTemplateQuery("carrega pessoas", pessoaDB, sql)
+	test := `
+SELECT
+	cpf, nome_completo, usuario, senha, email, data_criacao, data_modificacao, estado
+FROM
+	pessoa
+`
+	if query != test {
+		t.Error(query)
+	}
+}
+
 // TESTES ANTIGOS
 
 // var (
@@ -30,25 +50,5 @@ func TestGetDB02(t *testing.T) {
 
 // 	if err != nil {
 // 		t.Error("Não foi possível estabelecer conexão com o Banco de Dados", db)
-// 	}
-// }
-
-// func TestGetTemplateQuery(t *testing.T) {
-// 	sql := `
-// SELECT
-// 	{{.cpf}}, {{.nomeCompleto}}, {{.usuario}}, {{.senha}}, {{.email}}, {{.dataCriacao}}, {{.dataModificacao}}, {{.estado}}
-// FROM
-// 	{{.tabela}}
-// `
-
-// 	query := getTemplateQuery("carrega pessoas", pessoaDB, sql)
-// 	test := `
-// SELECT
-// 	cpf, nome_completo, usuario, senha, email, data_criacao, data_modificacao, estado
-// FROM
-// 	pessoa
-// `
-// 	if query != test {
-// 		t.Error(query)
 // 	}
 // }

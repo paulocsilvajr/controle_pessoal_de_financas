8070bac6b4e3d960cefdfe608fab47b820b8bb97
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-07-06 09:57:59 -0300

    Corrigido script de git push para usar variável BRANCH ao invés do nome da branch

--- API/v1/push_api-gorm.sh
+++ API/v1/push_api-gorm.sh
@@ -6,6 +6,6 @@ BRANCH="api-gorm"
 if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
     git push origin api-gorm && git push bitbucket api-gorm && git push github api-gorm
 else
-    echo "Altere para a branch '$BRANCH'(git checkout api-gorm) para executar esse script"
+    echo "Altere para a branch '$BRANCH'(git checkout '$BRANCH') para executar esse script"
     exit 1
 fi
--- API/v1/push_api.sh
+++ API/v1/push_api.sh
@@ -6,7 +6,7 @@ BRANCH="api"
 if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
     git push origin api && git push bitbucket api && git push github api
 else
-    echo "Altere para a branch '$BRANCH'(git checkout api) para executar esse script"
+    echo "Altere para a branch '$BRANCH'(git checkout '$BRANCH') para executar esse script"
     exit 1
 fi
 

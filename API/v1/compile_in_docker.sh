6d444eb4cf826ab2ecd143f23df8a8d1df3fe9eb
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-07-13 16:44:20 -0300

    Corrigido scripts para compilar a API via Docker

--- API/v1/compile_in_docker.sh
+++ API/v1/compile_in_docker.sh
@@ -3,9 +3,9 @@
 # ALTERE a variável a seguir com o diretório do projeto
 # controle_pessoal_de_financas de acordo com a localização no seu computador.
 # Geralmente localizado na pasta 'go' na home se seu usuário. Exemplo:
-# /home/seu-usuario/go/src/controle_pessoal_de_financas
-DIR_ABS=$HOME/go/src/controle_pessoal_de_financas
+# /home/seu-usuario/go/src/github.com/usuario_github/controle_pessoal_de_financas
+DIR_ABS=$HOME/go/src/github.com/paulocsilvajr/controle_pessoal_de_financas
 
-docker run -it --rm -v $DIR_ABS:/go/src/controle_pessoal_de_financas golang_custom:controle_pessoal_de_financas ./github.com/paulocsilvajr/controle_pessoal_de_financas/API/v1/compile_static_linux_amd64.sh
+docker run -it --rm -v $DIR_ABS:/go/src/controle_pessoal_de_financas golang_custom:controle_pessoal_de_financas ./API/v1/compile_static_linux_amd64.sh
 
 sudo chown -vR $USER $DIR_ABS/API/v1/bin

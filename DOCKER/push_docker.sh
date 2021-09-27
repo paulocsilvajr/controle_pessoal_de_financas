981bb9b2606f85c92b939a2303740c9bc5395b4a
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-09-27 20:49:55 -0300

    Adicionado script para enviar alterações para repositórios remotos GOG(origin), Github e Bitbucket

---
+++ DOCKER/push_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH="docker"
+
+if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
+    git push origin $BRANCH && git push bitbucket $BRANCH && git push github $BRANCH
+else
+    echo "Altere para a branch '$BRANCH'(git checkout '$BRANCH') para executar esse script"
+    exit 1
+fi
+

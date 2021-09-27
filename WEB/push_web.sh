7467f7a49cfcdb68466698bc896365b0950c2f5b
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-09-27 20:57:19 -0300

    Atualizado script para enviar para repositórios remotos

--- WEB/push_web.sh
+++ WEB/push_web.sh
@@ -1,1 +1,12 @@
-git push origin web && git push bitbucket web && git push github web
+#!/usr/bin/env bash
+
+CURRENTBRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH="web"
+
+if [ "$BRANCH" = "$CURRENTBRANCH" ]; then
+    git push origin $BRANCH && git push bitbucket $BRANCH && git push github $BRANCH
+else
+    echo "Altere para a branch '$BRANCH'(git checkout '$BRANCH') para executar esse script"
+    exit 1
+fi
+

41147fd5847f35491d2c4ed84f883e09f34cf822
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-11-19 14:25:57 -0300

    Adicionado controller Lancamento, para desacoplar as operações de lancamento de controle de conta

---
+++ WEB/app/controle-pessoal-financas-laravel/app/Http/Controllers/LancamentoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LancamentoController extends Controller
+{
+    public function index(Request $request)
+    {
+        # code...
+    }
+}

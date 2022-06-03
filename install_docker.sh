9287deada72a35cb9bb31c36449d3d201f4ec7ed
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-06-03 16:04:30 -0300

    Adicionado sudo para usermod em install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update &&
     sudo apt-get -y install docker docker-compose &&
-    usermod -aG docker $USER && echo -e "Adicionado grupo 'docker' no usuário $USER" &&
+    sudo usermod -aG docker $USER && echo -e "Adicionado grupo 'docker' no usuário $USER" &&
     su - $USER
